ecdf1ff56855473bea0916af27d6a436a7b2862e
Pavel Abramuk <pavel.abramuk5@gmail.com>
2022-08-17 09:48:31 +0300

    added separate selector for Sub Item menu button

--- test/pageobject/widget.page.js
+++ test/pageobject/widget.page.js
@@ -5,13 +5,17 @@ class WidgetPage extends BasePage {
     return $(`.//a[contains(text(),'${text}')]`);
   }
 
+  getSubItemButton(text) {
+    return $$(`.//a[contains(text(),'${text}')]`)[0];
+  }
+
+  getTab(text) {
+    return $(`.//a[text()="${text}"]`);
+  }
+
   getTabContent(tab) {
     const tabLowered = tab.toLowerCase();
     return $(`.//div[contains(@id, '${tabLowered}')]`);
-  }
-  
-  getTab(text) {
-    return $(`.//a[text()="${text}"]`);
   }
 }
 
